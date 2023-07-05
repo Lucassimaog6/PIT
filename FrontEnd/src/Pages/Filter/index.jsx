a9dce3d082c10bc3a2f8ec9110efaff1060d2da4
AnthonyLomasso <105284519+AnthonyJLM@users.noreply.github.com>
2023-07-05 20:53:42 -0300

    Update index.jsx

--- FrontEnd/src/Pages/Filter/index.jsx
+++ FrontEnd/src/Pages/Filter/index.jsx
@@ -36,7 +36,7 @@ export default function Filter() {
 	return (
 		<main>
 			<aside>
-				<div className='flex flex-col w-100% h-80px bg-white'>
+				<div className='flex flex-col w-100% h-80px bg-amber-500 text-black'>
 					<h1 className='text-xl'>Dificuldade: </h1>
 					<label>
 						<input
