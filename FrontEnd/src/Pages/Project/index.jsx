87b31f15169c8eb152cee406b68bff1dc0f4a29e
AnthonyLomasso <105284519+AnthonyJLM@users.noreply.github.com>
2023-07-05 19:58:23 -0300

    Update index.jsx

--- FrontEnd/src/Pages/Project/index.jsx
+++ FrontEnd/src/Pages/Project/index.jsx
@@ -85,7 +85,7 @@ export default function Project() {
 						</button>
 						<button type="button"
 								onClick={() => setDificulty("3")}
-								className={`${dificulty === "3" ? 'bg-amber-400' : ''} px-4 py-2 w-1/3 text-sm font-medium text-white border border-gray-200 rounded-r-md`}>
+								className={`${dificulty === "3" ? 'bg-amber-400' : ''} px-4 py-2 w-1/3 text-sm font-medium text-white border border-gray-200 rounded-md`}>
 							Difícil
 						</button>
 					</div>
@@ -97,7 +97,7 @@ export default function Project() {
 				>
 					Descrição:
 					<textarea
-						className='p-2 bg-black/20 rounded '
+						className='p-2 bg-black/20 rounded-md '
 						name='description'
 						id='description'
 						cols='30'
@@ -109,7 +109,7 @@ export default function Project() {
 				<label htmlFor='linkImage'>
 					Link da imagem:
 					<input
-						className='w-full p-2 bg-black/20 rounded'
+						className='w-full p-2 bg-black/20 rounded-md'
 						type='text'
 						id='linkImage'
 						ref={linkImage}
@@ -117,7 +117,7 @@ export default function Project() {
 				</label>
 
 				<button
-					className='w-full p-2 bg-black/40 rounded bg-fuchsia-950'
+					className='w-full p-2 bg-black/40 rounded bg-fuchsia-950 rounded-md'
 					onClick={handleNewProject}
 				>
 					Enviar
