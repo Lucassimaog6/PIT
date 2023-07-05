65ecdce49f6aab16835cebe06df85732db34f509
Amin Morais <111761627+dev1amin@users.noreply.github.com>
2023-07-05 11:38:57 -0300

    Update Card.Project.jsx

--- FrontEnd/src/Components/Card.Project.jsx
+++ FrontEnd/src/Components/Card.Project.jsx
@@ -26,7 +26,7 @@ export default function CardProject(params) {
 
     return (
         <section className='bg-amber-500 p-4 rounded-xl flex flex-col gap-2 h-96'>
-         <img src={imagempro} alt="Imagem Projeto"/>
+         <img class="h-48" src={imagempro} alt="Imagem Projeto"/>
             <h1 className='text-4xl flex flex-wrap items-center gap-2'>
                 {title}
                 <div className={`${dificultyColor} rounded-full w-6 h-6 flex items-center justify-center`}>
