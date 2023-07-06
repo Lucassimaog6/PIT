932bb64212ca9ea5bd94715f0fbedde8fc9bccff
Amin Morais <111761627+dev1amin@users.noreply.github.com>
2023-07-05 23:20:59 -0300

    Update index.jsx

--- FrontEnd/src/Pages/Project/index.jsx
+++ FrontEnd/src/Pages/Project/index.jsx
@@ -47,7 +47,7 @@ export default function Project() {
 					onClick={() => navigate('/home')}>Voltar
 			</button>
 
-			<main className='flex flex-col gap-2'>
+			<main className='flex flex-col gap-2 p-16 bg-amber-500 rounded-lg'>
 
 				<h1 className='text-center text-6xl'>Novo Projeto</h1>
 				<label htmlFor='title'>
@@ -75,17 +75,17 @@ export default function Project() {
 					<div className="inline-flex rounded-md shadow-sm">
 						<button type="button"
 								onClick={() => setDificulty("1")}
-								className={`${dificulty === "1" ? 'bg-amber-400' : ''} px-4 py-2 w-1/3 text-sm font-medium text-white border border-gray-200 rounded-l-lg bg-amber-500`}>
+								className={`${dificulty === "1" ? 'bg-purple-600' : ''} px-4 py-2 w-1/3 text-sm font-medium text-white border border-gray-200 rounded-l-lg `}>
 							Fácil
 						</button>
 						<button type="button"
 								onClick={() => setDificulty("2")}
-								className={`${dificulty === "2" ? 'bg-amber-400' : ''} px-4 py-2 w-1/3 text-sm font-medium text-white border-t border-b border-gray-200 bg-amber-500`}>
+								className={`${dificulty === "2" ? 'bg-purple-600' : ''} px-4 py-2 w-1/3 text-sm font-medium text-white border-t border-b border-gray-200 `}>
 							Médio
 						</button>
 						<button type="button"
 								onClick={() => setDificulty("3")}
-								className={`${dificulty === "3" ? 'bg-amber-400' : ''} px-4 py-2 w-1/3 text-sm font-medium text-white border border-gray-200 rounded-r-md bg-amber-500`}>
+								className={`${dificulty === "3" ? 'bg-purple-600' : ''} px-4 py-2 w-1/3 text-sm font-medium text-white border border-gray-200 rounded-r-md `}>
 							Difícil
 						</button>
 					</div>
@@ -117,7 +117,7 @@ export default function Project() {
 				</label>
 
 				<button
-					className='w-full p-2 bg-black/40 rounded bg-fuchsia-950 rounded-md'
+					className='w-full p-2 bg-black/40 rounded bg-purple-600 rounded-md'
 					onClick={handleNewProject}
 				>
 					Enviar
