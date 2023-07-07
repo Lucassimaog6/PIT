8c517f12750d72ae4ab71724562e5d9713155c43
Lucas Ferreira Simão <22101918@aluno.cotemig.com.br>
2023-07-07 19:14:30 -0300

    Update ProjectID.jsx

--- FrontEnd/src/Pages/Project/ProjectID.jsx
+++ FrontEnd/src/Pages/Project/ProjectID.jsx
@@ -1,33 +1,66 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import {useNavigate} from 'react-router-dom';
+import {useEffect, useState} from "react"
+import {useParams, useNavigate} from "react-router-dom"
 
 export default function ProjectID() {
-    const { id } = useParams()
+    const {id} = useParams()
     const [project, setProject] = useState()
+    const [dificultyText, setDificultyText] = useState()
+    const [dificultyColor, setDificultyColor] = useState()
+    const [owner, setOwner] = useState()
     const navigate = useNavigate();
-    
+
     useEffect(() => {
         (async () => {
             const response = await fetch(`${import.meta.env.VITE_API_URL}/projects/${id}`);
             const data = await response.json()
             setProject(data)
+
+            switch (data.dificulty) {
+                case "1":
+                    setDificultyColor('bg-green-400');
+                    setDificultyText('I');
+                    break;
+                case "2":
+                    setDificultyColor('bg-orange-600');
+                    setDificultyText('II');
+                    break;
+                case "3":
+                    setDificultyColor('bg-red-400');
+                    setDificultyText('III');
+                    break;
+            }
+            getOwner(data.owner)
         })()
     }, [])
+
+    async function getOwner(ownerId) {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/users/${ownerId}`);
+        const data = await response.json()
+        setOwner(data.name)
+    }
 
 
     return (
         project ? (
-            <main>
-                <button className='bg-amber-500 py-1 px-2 rounded'
-                    onClick={() => navigate('/home')}>Voltar
+            <>
+                <button className='m-2 bg-amber-500 py-1 px-2 rounded'
+                        onClick={() => navigate('/home')}>Voltar
                 </button>
-                <p className="mt-5">Descrição do projeto: </p>
-                <p>{project.description}</p>
-                <p>Nível: </p>
-                <p>{project.dificulty}</p>
-            </main>
-        
+                <main className='min-h-screen grid items-center justify-center'>
+                    <div className='bg-white/20 p-4 rounded-lg'>
+                        <h1 className='text-6xl'>Título: {project.title}</h1>
+                        <p className='text-2xl'>Descrição do projeto: {project.description}</p>
+                        <label className='flex gap-2'>Nível:
+                            <div className={`${dificultyColor} rounded-full w-6 h-6 flex items-center justify-center`}>
+                                <span className='text-sm font-serif'>{dificultyText}</span>
+                            </div>
+                        </label>
+                        <p className='text-2xl'>Stack: {project.tags[0] === 'back' ? 'BackEnd' : 'FrontEnd'}</p>
+                        <p>Autor: {owner}</p>
+                    </div>
+                </main>
+            </>
+
         ) : (
             <h1>carregando</h1>
         )
