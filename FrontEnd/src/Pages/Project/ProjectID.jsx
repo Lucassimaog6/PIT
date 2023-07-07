84a0b3ac493cb3da414a343dde8e2df18bb95cdd
Amin Morais <111761627+dev1amin@users.noreply.github.com>
2023-07-07 21:53:43 +0000

    projeto

--- FrontEnd/src/Pages/Project/ProjectID.jsx
+++ FrontEnd/src/Pages/Project/ProjectID.jsx
@@ -22,10 +22,8 @@ export default function ProjectID() {
                 <button className='bg-amber-500 py-1 px-2 rounded'
                     onClick={() => navigate('/home')}>Voltar
                 </button>
-                <p className="mt-5">Descrição do projeto: </p>
-                <p>{project.description}</p>
-                <p>Nível: </p>
-                <p>{project.dificulty}</p>
+                <p className="mt-5">Descrição do projeto: {project.description}</p>
+                <p>Nível: {project.dificulty}</p>
             </main>
         
         ) : (
