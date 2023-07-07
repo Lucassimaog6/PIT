b4057bdaf2013b972b572dd9c19f0ab2b2b4a39b
Amin Morais <111761627+dev1amin@users.noreply.github.com>
2023-07-07 20:56:59 +0000

    teste3

--- FrontEnd/src/Pages/Project/ProjectID.jsx
+++ FrontEnd/src/Pages/Project/ProjectID.jsx
@@ -17,24 +17,10 @@ export default function ProjectID() {
     return (
         project ? (
             <main>
-                <section>
-            <img
-                className="h-48" src={imagempro} alt="Imagem Projeto" />
-            <h1 className='text-4xl flex flex-wrap items-center gap-2'>
-                {title}
-                <div className={`${dificultyColor} rounded-full w-6 h-6 flex items-center justify-center`}>
-                    <span className='text-sm font-serif'>{dificultyText}</span>
-                </div>
-            </h1>
-            <div className='flex gap-2'>
-                <span className='bg-purple-600 rounded-full text-sm py-0.5 px-3'>Tecnologia</span>
-                <span className='bg-purple-600 rounded-full text-sm py-0.5 px-3'>Linguagem</span>
-                <span className='bg-purple-600 rounded-full text-sm py-0.5 px-3'>Framework</span>
-            </div>
-            <p>
-                {description}
-            </p>
-        </section>
+                <button className='bg-amber-500 py-1 px-2 rounded'
+                    onClick={() => navigate('/home')}>Voltar
+                </button>
+                <div>Seu projeto</div>
             </main>
         
         ) : (
